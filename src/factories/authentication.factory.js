8c43c169f3802484bf5817143d899bba98c7065e
Simone Gizzi <simonegizzi85@gmail.com>
2018-09-17 00:37:20 +0200

    - split components into view and controller, managed with states - comment session interceptor (now unused) - completed basic authentication mechanism

--- src/factories/authentication.factory.js
+++ src/factories/authentication.factory.js
@@ -7,15 +7,33 @@
         .factory('authentication', ['eventHandler', authentication]);
 
     function authentication(eventHandler) {
-        let authorized = false;
-        let credentials = {};
-        return {
-            checkCredentials: function(credentials) {
-                if(!credentials.hasOwnProperty('username') || !credentials.hasOwnProperty('password')) {
-                    authorized = false;
-                    eventHandler.emit('missingCredentials');
+        function checkLocalStorage(){
+            if (typeof localStorage !== 'undefined') {
+                try {
+                    localStorage.setItem('localstorage', 'localstorage');
+                    if (localStorage.getItem('localstorage') === 'localstorage') {
+                        localStorage.removeItem('localstorage');
+                        return true;
+                    } else {
+                        // localStorage is disabled
+                        return false;
+                    }
+                } catch(e) {
+                    // localStorage is disabled
+                    return false;
                 }
-
+            } else {
+                // localStorage is not available
+                return false;
+            }
+        }
+        return {
+            setCredentials: function(credentials) {
+                if (checkLocalStorage()){
+                    localStorage.setItem('username', credentials.username);
+                    localStorage.setItem('password', credentials.password);
+                    eventHandler.emit('User Log-in');
+                }
             }
         }
     }
