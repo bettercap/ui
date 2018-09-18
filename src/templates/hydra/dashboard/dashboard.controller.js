ad820ff88f829a105490f0b277331ff14cd42e85
Simone Gizzi <simonegizzi85@gmail.com>
2018-09-19 01:57:42 +0200

    - implements button for switch from normal mode to hydra mode

--- src/templates/hydra/dashboard/dashboard.controller.js
+++ src/templates/hydra/dashboard/dashboard.controller.js
@@ -12,22 +12,37 @@
          */
         $scope.hosts = [];
         $scope.edges = [];
+        $scope.hydraModeEnabled = false;
+        $scope.hydraBtnHover = false;
 
         /**
          * Listeners
          */
-        $scope.$on('sessionReady', function(){
+        $scope.$on('sessionReady', function() {
             $scope.hosts = networkHandler.getHosts();
             $scope.edges = networkHandler.getEdges($scope.hosts);
         });
 
+        /**
+         * Methods
+         */
+        $scope.hydraMode = function() {
+            $scope.hydraModeEnabled = !$scope.hydraModeEnabled;
+        };
+        $scope.enableHydraBtnHover = function() {
+            $scope.hydraBtnHover = true;
+        };
+        $scope.disableHydraBtnHover = function() {
+            $scope.hydraBtnHover = false;
+        };
+
         //TODO: porkaround temporaneo, necessario refactoring in favore di una direttiva
-        function setHydraWrapperHeight(){
+        function setHydraWrapperHeight() {
             let windowHeight = $window.innerHeight;
             let headerHeight = document.querySelector('#hydraMainHeader').offsetHeight;
             document.querySelector('#hydraWrapper').style.height = windowHeight - headerHeight + 'px';
         }
-        $timeout(function(){
+        $timeout( function() {
             setHydraWrapperHeight();
         }, 500);
         $window.addEventListener('resize', setHydraWrapperHeight);
