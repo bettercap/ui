60c74c3eefa9ac9f095252733c26a42a5bb82ca0
Simone Gizzi <simonegizzi85@gmail.com>
2018-11-13 22:30:51 +0100

    [update] some various update after bettercap update

--- app.config.js
+++ app.config.js
@@ -6,7 +6,7 @@
         .module('Hydra')
         .config(['$httpProvider', hydraConfiguration])
         .constant('configuration', {
-            apiEndpoint: 'https://127.0.0.1:8081/api/',
+            apiEndpoint: 'http://192.168.1.2:8081/api/',
             appVersion: '0.0.1'
         });
 
