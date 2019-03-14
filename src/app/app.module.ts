bf1a3d75cfa8f1c612d7c0627134791d4df7d4cb
evilsocket <evilsocket@gmail.com>
2019-03-14 21:35:32 +0100

    started working on the advanced tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,18 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
-import { OmnibarComponent } from './components/omnibar/omnibar.component';
-// import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { EventsTableComponent } from './components/events-table/events-table.component';
 import { EventComponent } from './components/event/event.component';
 import { LanTableComponent } from './components/lan-table/lan-table.component';
+import { WifiTableComponent } from './components/wifi-table/wifi-table.component';
 import { BleTableComponent } from './components/ble-table/ble-table.component';
 import { HidTableComponent } from './components/hid-table/hid-table.component';
-import { WifiTableComponent } from './components/wifi-table/wifi-table.component';
-// import { ModuleOptionsComponent } from './components/module-options/module-options.component';
-import { SignalIndicatorComponent } from './components/signal-indicator/signal-indicator.component';
-// import { TerminalComponent } from './components/terminal/terminal.component';
+import { AdvancedComponent } from './components/advanced/advanced.component';
 
+import { SignalIndicatorComponent } from './components/signal-indicator/signal-indicator.component';
 import { SortableColumnComponent } from './components/sortable-column/sortable-column.component';
+import { OmnibarComponent } from './components/omnibar/omnibar.component';
+
 import { SearchPipe } from './components/search.pipe';
 import { AlivePipe } from './components/alive.pipe';
 
@@ -31,18 +30,17 @@ import { AlivePipe } from './components/alive.pipe';
         AppComponent,
         LoginComponent,
         MainHeaderComponent,
-        OmnibarComponent,
-        // SidebarComponent,
+
         EventsTableComponent,
         EventComponent,
         LanTableComponent,
         BleTableComponent,
         HidTableComponent,
         WifiTableComponent,
-        // ModuleOptionsComponent,
-        SignalIndicatorComponent,
-        // TerminalComponent
+        AdvancedComponent,
         
+        OmnibarComponent,
+        SignalIndicatorComponent,
         SortableColumnComponent,
         SearchPipe,
         AlivePipe
