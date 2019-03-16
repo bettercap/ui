8dd0a62861ed9f5da0450488370b8abb83428846
evilsocket <evilsocket@gmail.com>
2019-03-16 13:49:33 +0100

    added cool notification system

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,7 +1,8 @@
 import {
     Component, 
-    EventEmitter, 
     OnInit, 
+    ComponentFactoryResolver,
+    Injector
 } from '@angular/core';
 import {Router} from '@angular/router';
 import { ToastrService } from 'ngx-toastr';  
@@ -20,6 +21,7 @@ declare var $: any;
 export class MainHeaderComponent implements OnInit {
     faSignOutAlt = faSignOutAlt;
 
+    apiFirstUpdate: boolean = true;
     numEvents = 0;
     numHosts = 0;
     numAps = 0;
@@ -31,7 +33,13 @@ export class MainHeaderComponent implements OnInit {
     sessionError: any;
     commandError: any;
 
-    constructor(private api: ApiService, private router: Router, private toastr: ToastrService) { 
+    constructor(
+        private api: ApiService, 
+        private router: Router, 
+        private toastr: ToastrService,
+        private resolver: ComponentFactoryResolver, 
+        private injector: Injector
+    ) { 
         this.updateSession(this.api.session);
         this.updateEvents(this.api.events, true);
     }
@@ -42,7 +50,8 @@ export class MainHeaderComponent implements OnInit {
         });
 
         this.api.onNewEvents.subscribe(events => {
-            this.updateEvents(events);
+            this.updateEvents(events, this.apiFirstUpdate);
+            this.apiFirstUpdate = false;
         });
 
         this.api.onSessionError.subscribe(error => {
@@ -66,27 +75,88 @@ export class MainHeaderComponent implements OnInit {
         this.numHID = session.hid['devices'].length; 
     }
 
-    private onModuleEvent(event, firstUpdate: boolean) {
-        let modName = event.data;
-        let evKey = event.tag + "::" + modName + "::" + event.time;
+    private eventCacheKey(event) {
+        let key = event.tag + "::";
+        if( typeof event.data == 'string' ) 
+            key += event.data + "::";
+        else
+            key += "{}::";
+        key += event.time;
+        return key;
+    }
+
+    private eventHTML(event) : string {
+        // reuse the EventComponent at runtime to get the same HTML
+        // we'd have in the event table ... reusability FTW ^_^
+        const factory = this.resolver.resolveComponentFactory(EventComponent);
+        const component = factory.create(this.injector); 
+
+        component.instance.event = event;
+        component.changeDetectorRef.detectChanges();
+
+        return component.location.nativeElement.innerHTML;
+    }
+
+    private isTrackedEvent(event) : boolean {
+        // modules start and stop events
+        if( event.tag.indexOf('mod.') == 0 )
+            return true;
+        // some recon module got a new target
+        if( event.tag.indexOf('.new') != -1 )
+            return true;
+        // wifi l00t
+        if( event.tag == 'wifi.client.handshake' )
+            return true;
+
+        return false;
+    }
+
+    private eventClass(event) : string {
+        if( event.tag == 'mod.started' || event.tag.indexOf('.new') != -1 )
+            return 'toast-success';
+        
+        else if( event.tag == 'mod.stopped' || event.tag == 'wifi.client.handshake' )
+            return 'toast-warning';
+
+        return 'toast-info';
+    }
+
+    private handleTrackedEvent(event, firstUpdate: boolean) {
+        // since we're constantly polling for /api/events and we'll
+        // end up getting the same events we already shown in multiple
+        // requests, we need to "cache" this information to avoid showing
+        // the same notification more than once.
+        let evKey = this.eventCacheKey(event);
         if( evKey in this.modNotificationCache === false ) {
             this.modNotificationCache[evKey] = true;
+            // first time we get the event we don't want to notify the user,
+            // otherwise dozens of notifications might be generated after
+            // a page refresh
             if( firstUpdate == false ) {
-                if( event.tag == 'mod.started' ) {
-                    this.toastr.success(modName + " module started.");
-                } else {
-                    this.toastr.warning(modName + " module stopped.");
-                }
+                this.toastr.show(
+                    this.eventHTML(event), 
+                    event.tag, 
+                    { 
+                        // any dangerous stuff should already be escaped by the 
+                        // EventComponent anyway ... i hope ... ... i hope ...
+                        enableHtml: true,
+                        toastClass: 'ngx-toastr ' + this.eventClass(event)
+                    }
+                );
             }
         }
     }
 
     private updateEvents(events, firstUpdate: boolean = false) {
         this.numEvents = events.length;
-        for( let i = 0; i < this.numEvents; i++ ) {
-            let event = events[i];
-            if( event.tag.indexOf('mod.') == 0 ) {
-                this.onModuleEvent(event, firstUpdate);
+        if( this.numEvents == 0 ){
+            this.toastr.clear();
+        } else {
+            for( let i = 0; i < this.numEvents; i++ ) {
+                let event = events[i];
+                if( this.isTrackedEvent(event) ) {
+                    this.handleTrackedEvent(event, firstUpdate);
+                }
             }
         }
     }
